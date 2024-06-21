acea1d0503aaea1c2b52cea43762576df86c5748
ighenriquez <120124797+ighenriquez@users.noreply.github.com>
2024-06-21 15:12:02 -0400

    Add files via upload

---
+++ pregunta 6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int m = 3;
+    vector<int> boxes = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+    int n = boxes.size();
+
+    sort(boxes.rbegin(), boxes.rend());
+
+    vector<int> drivers(m, 0);
+    for (const auto& box : boxes) {
+        int minLoadIndex = min_element(drivers.begin(), drivers.end()) - drivers.begin();
+        drivers[minLoadIndex] += box;
+    }
+
+    int maxLoad = *max_element(drivers.begin(), drivers.end());
+    cout << "Maximum load for a driver: " << maxLoad << endl;
+
+    return 0;
+}
